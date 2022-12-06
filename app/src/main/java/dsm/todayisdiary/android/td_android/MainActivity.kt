438430e779a7986e0af70344fec3dabd12e0e8df
이준범 <ljb050310@naver.com>
2022-12-06 20:27:22 +0900

    ⚙️:: 프로젝트 기본 세팅

---
+++ app/src/main/java/dsm/todayisdiary/android/td_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package dsm.todayisdiary.android.td_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
