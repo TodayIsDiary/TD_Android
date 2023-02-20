10d0da0691f440fc3c96d0194ef2522fa40ea95e
이준범 <ljb050310@naver.com>
2023-02-20 18:22:20 +0900

    ➕:: SignUp Profile Activity 추가

---
+++ app/src/main/java/dsm/todayisdiary/android/td_android/SignUpProfileActivity.kt
@@ -0,0 +1,12 @@
+package dsm.todayisdiary.android.td_android
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_profile)
+    }
+}
