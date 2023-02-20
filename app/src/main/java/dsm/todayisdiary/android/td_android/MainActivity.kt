a6133bbfb17d2d2c191a9272439183948b249178
이준범 <ljb050310@naver.com>
2023-02-20 18:22:33 +0900

    ➕:: Main Activity 추가

--- app/src/main/java/dsm/todayisdiary/android/td_android/MainActivity.kt
+++ app/src/main/java/dsm/todayisdiary/android/td_android/MainActivity.kt
@@ -1,11 +1,23 @@
 package dsm.todayisdiary.android.td_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dsm.todayisdiary.android.td_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.todayisdiaryLoginMain.setOnClickListener {
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java));
+        }
+        binding.signupMain.setOnClickListener {
+            startActivity(Intent(this@MainActivity, SignUpActivity::class.java));
+        }
     }
+
 }
