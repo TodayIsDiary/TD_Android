61ec613bceb5223fed436524e907479e00e0c4c6
이준범 <ljb050310@naver.com>
2023-05-07 22:48:33 +0900

    ➕:: UserEmailRequest 추가

--- app/src/main/java/dsm/todayisdiary/android/td_android/data/request/user/UserEmailRequest.kt
+++ app/src/main/java/dsm/todayisdiary/android/td_android/data/request/user/UserEmailRequest.kt
@@ -3,5 +3,5 @@ package dsm.todayisdiary.android.td_android.data.request.user
 import com.google.gson.annotations.SerializedName
 
 data class UserEmailRequest(
-    @SerializedName("email") val email: String,
+    @SerializedName("email") val email: String
 )
