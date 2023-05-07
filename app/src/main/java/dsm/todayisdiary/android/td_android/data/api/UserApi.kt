8f82ee312da18b64d19ba82e85ae7756824e1af1
이준범 <ljb050310@naver.com>
2023-05-07 22:47:20 +0900

    ➕:: user signup, check 등 API 추가

--- app/src/main/java/dsm/todayisdiary/android/td_android/data/api/UserApi.kt
+++ app/src/main/java/dsm/todayisdiary/android/td_android/data/api/UserApi.kt
@@ -1,21 +1,31 @@
 package dsm.todayisdiary.android.td_android.data.api
 
+import dsm.todayisdiary.android.td_android.data.request.user.UserEmailCheckRequest
 import dsm.todayisdiary.android.td_android.data.request.user.UserEmailRequest
 import dsm.todayisdiary.android.td_android.data.request.user.UserLoginRequest
 import dsm.todayisdiary.android.td_android.data.request.user.UserSignUpRequest
-import dsm.todayisdiary.android.td_android.data.response.UserLoginResponse
+import dsm.todayisdiary.android.td_android.data.response.user.UserLoginResponse
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface UserApi {
 
     @POST("user/login")
     fun userLogin(@Body body: UserLoginRequest): Call<UserLoginResponse>
 
-    @POST("user/signup")
-    fun userSignUp(@Body body: UserSignUpRequest)
+    @POST("user/signup/add")
+    fun userSignUp(@Body body: UserSignUpRequest): Call<ResponseBody>
 
     @POST("user/email")
-    fun userEmail(@Body body: UserEmailRequest)
+    fun userEmail(@Body body: UserEmailRequest): Call<ResponseBody>
+
+    @POST("user/signup/check")
+    fun userEmailCheck(@Body body: UserEmailCheckRequest): Call<Boolean>
+
+    @GET("user/check")
+    fun userIdCheck(@Query("accountId") accountId: String): Call<Boolean>
 }
